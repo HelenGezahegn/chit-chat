83f61fbbf245b11540c372e3cd2ae0d5c2205726
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-29 04:35:03 -0600

    chore: basic layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import "./App.css";
+import { ChannelContainer, ChannelListContainer } from "./components";
+
+const apiKey = "aken6unva5k6";
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      <h1>Medical Pager Chat App</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer
+        //   isCreating={isCreating}
+        //   setIsCreating={setIsCreating}
+        //   setCreateType={setCreateType}
+        //   setIsEditing={setIsEditing}
+        />
+        <ChannelContainer
+        //   isCreating={isCreating}
+        //   setIsCreating={setIsCreating}
+        //   isEditing={isEditing}
+        //   setIsEditing={setIsEditing}
+        //   createType={createType}
+        />
+      </Chat>
     </div>
   );
 };
