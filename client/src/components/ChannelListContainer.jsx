0124de48c9bdd8694d761da8425b7ead6b04278d
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-29 05:15:13 -0600

    feat: added channel + DM style skeleton

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -37,6 +37,19 @@ const ChannelContainer = () => {
           filters={{}}
           channelRenderFilterFn={() => {}}
           List={(listProps) => <TeamChannelList {...listProps} type="team" />}
+          Preview={(previewProps) => (
+            <TeamChannelPreview {...previewProps} type="team" />
+          )}
+        />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => (
+            <TeamChannelList {...listProps} type="messaging" />
+          )}
+          Preview={(previewProps) => (
+            <TeamChannelPreview {...previewProps} type="messaging" />
+          )}
         />
       </div>
     </>
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,11 +3,13 @@ import ChannelContainer from "./ChannelContainer";
 import ChannelListContainer from "./ChannelListContainer";
 import ResultsDropdown from "./ResultsDropdown";
 import TeamChannelList from "./TeamChannelList";
+import TeamChannelPreview from "./TeamChannelPreview";
 
 export {
   ChannelSearch,
   ChannelContainer,
   ChannelListContainer,
   ResultsDropdown,
-  TeamChannelList
+  TeamChannelList,
+  TeamChannelPreview
 };
