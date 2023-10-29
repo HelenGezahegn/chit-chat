161a9fd1c59d5a78b15a95294a91b6acc274ed62
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-29 05:02:52 -0600

    feat: channel search bar + team channel list skeleton

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,13 @@
+import ChannelSearch from "./ChannelSearch";
 import ChannelContainer from "./ChannelContainer";
 import ChannelListContainer from "./ChannelListContainer";
+import ResultsDropdown from "./ResultsDropdown";
+import TeamChannelList from "./TeamChannelList";
 
-export { ChannelContainer, ChannelListContainer };
+export {
+  ChannelSearch,
+  ChannelContainer,
+  ChannelListContainer,
+  ResultsDropdown,
+  TeamChannelList
+};
